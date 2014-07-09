9a2061c27f0dd6589cd2113188f772f4e9b79c4a
Jamie McCrindle <jamiemccrindle@gmail.com>
2014-07-09 20:51:09 +0100

    docker london

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello World!")
+			fmt.Fprintf(w, "Hello Docker London!")
 		})
 	http.ListenAndServe(":8000", nil)
 }

92ece75bdb8d9e405a5b23860a3ce31bba265e52
Jamie McCrindle <jamiemccrindle@gmail.com>
2014-07-09 17:34:09 +0100

    hello world

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello Docker and Ansible!")
+			fmt.Fprintf(w, "Hello World!")
 		})
 	http.ListenAndServe(":8000", nil)
 }
